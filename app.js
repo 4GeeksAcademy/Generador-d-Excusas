6e6d6d1e58a06d880d09e37b8bddd626387190c5
Martin <90578316+MartinParafita@users.noreply.github.com>
2025-06-15 19:03:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car', 'my job'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+function selector(who,action,what,when) {
+    let excusaNueva = ""
+    let random = Math.floor(Math.random()*4);
+    excusaNueva += (who[random] + " ")
+    let random = Math.floor(Math.random()*4);
+    excusaNueva += (action[random] + " ")
+    let random = Math.floor(Math.random()*4);
+    excusaNueva += (what[random] + " ")
+    let random = Math.floor(Math.random()*4);
+    excusaNueva += (when[random] + " ")
+    return excusaNueva
+}
+let excusa = selector(who,action,what,when)
+let parrafoHtml = document.getElementById("excuse");
+parrafoHtml.innerHTML = excusa
